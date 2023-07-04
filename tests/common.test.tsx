d6caf0d94a03efa0a8350daa747d58c34bfa0a3c
handwoo24 <handwoo24@gmail.com>
2023-07-04 10:13:14 +0900

    :sparkles: add test pdf url

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -7,6 +7,7 @@ import { Doc } from '../src'
 
 describe('Common render', () => {
   it('renders without crashing', () => {
-    render(<Doc />)
+    const url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf'
+    render(<Doc url={url} />)
   })
 })
